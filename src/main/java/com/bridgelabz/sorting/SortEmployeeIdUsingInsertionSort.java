a52d5d5075ebb25bde2f99275ef645372748ea2c
poojangit <poojang147@gmail.com>
2025-06-10 10:53:16 +0530

    [POOJA] Insertion-Sort : Implemented Insertion sort for sorting the employee IDs

--- src/main/java/com/bridgelabz/sorting/SortEmployeeIdUsingInsertionSort.java
+++ src/main/java/com/bridgelabz/sorting/SortEmployeeIdUsingInsertionSort.java
@@ -1,32 +1,33 @@
 package com.bridgelabz.sorting;
 
-public class InsertionSort {
+import java.util.Arrays;
+
+public class SortEmployeeIdUsingInsertionSort {
     //main method
     public static void main(String[] args) {
         //declare and initialize the arrays
-        int[] arr = {4,1,9,5,0,2};
+        int[] empIDs = {17,19,12,15,10,16};
         //method calling
-        insertionSort(arr);
-        for (int j : arr) {
-            System.out.print(j + " ");
-        }
+        System.out.println("Employee ID Before Sorting : " + Arrays.toString(empIDs));
+        insertionSort(empIDs);
+        System.out.println("Employee ID after Sorting: " + Arrays.toString(empIDs));
     }
-    public static void insertionSort(int[] arr){
+    public static void insertionSort(int[] empID){
         //find the length of the array
-        int n = arr.length;
+        int n = empID.length;
         //loop start from 1 because the first element with index 0 is already sorted
         for(int i=1; i<n; i++){
             //key is the element we are inserting it to the sorted part
-            int key = arr[i];
+            int key = empID[i];
             int j = i-1; // j is the index we are comparing it with the key elements
 
             //while elements are greater than key shift them one position to right
-            while(j>=0 && arr[j] > key){
-                arr[j+1] = arr[j];
+            while(j>=0 && empID[j] > key){
+                empID[j+1] = empID[j];
                 j--;
             }
             //place the key in the current position once all the elements are shifted
-            arr[j+1] = key;
+            empID[j+1] = key;
         }
 
     }
