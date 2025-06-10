8fbba388840c36c34c9c0796eb99c9dd4471d89e
poojangit <poojang147@gmail.com>
2025-06-10 11:02:59 +0530

    [POOJA] MergeSort : Implemented Merge sort for sorting the array of book prices

--- src/main/java/com/bridgelabz/sorting/SortBookPricesUsingMergeSort.java
+++ src/main/java/com/bridgelabz/sorting/SortBookPricesUsingMergeSort.java
@@ -2,15 +2,15 @@ package com.bridgelabz.sorting;
 
 import java.util.Arrays;
 
-public class MergeSort {
+public class SortBookPricesUsingMergeSort {
     //recursive method for splitting the array
-    public static void mergeSort(int[] arr, int left, int right) {
+    public static void mergeSort(int[] price, int left, int right) {
         if (left < right) {
             int mid = (left + right) / 2; //find the middle index
             //recursively sort left and right halves
-            mergeSort(arr, left, mid);
-            mergeSort(arr, mid +1, right);
-            merge(arr, left, mid, right);
+            mergeSort(price, left, mid);
+            mergeSort(price, mid +1, right);
+            merge(price, left, mid, right);
         }
     }
         private static void merge (int[] arr, int left, int mid, int right){
@@ -20,8 +20,7 @@ public class MergeSort {
             int[] leftArr = new int[n1];
             int[] rightArr = new int[n2];
             //copy values from an original array into L
-            for(int i=0;i<n1;i++)
-                leftArr[i] = arr[left + i];
+            System.arraycopy(arr, left, leftArr, 0, n1);
             //copy values into R
             for(int j=0; j<n2;j++)
                 rightArr[j] = arr[mid+1+j];
@@ -56,9 +55,10 @@ public class MergeSort {
 
     //main method
     public static void main(String[] args) {
-        int[] arr = {5,3,8,4,2,7}; //input array
-        mergeSort(arr,0,arr.length-1);
+        int[] bookPrices = {500,3000,800,400,200,700}; //input array
+        System.out.println("Array of Book prices Before Sorting : "+Arrays.toString(bookPrices));
+        mergeSort(bookPrices,0,bookPrices.length-1);
         //print the sorted array
-        System.out.println(Arrays.toString(arr));
+        System.out.println("After sorting : "+Arrays.toString(bookPrices));
     }
 }
